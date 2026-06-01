defed41354c1de668bf4781d43ce4dd919f2c93f
bodhekarvarad <bodhekarvarad2004@gmail.com>
2026-06-01 22:41:51 +0530

    alt msg

--- frontend/src/landing_page/product/Universe.js
+++ frontend/src/landing_page/product/Universe.js
@@ -13,6 +13,7 @@ function Universe() {
           <img
             src="media/images/smallcaseLogo.png"
             style={{ height: "44px" }}
+            alt="smallcaseLogo"
           />
           <p className="text-small text-muted">
             Thematic investing platform that helps you invest in diversified
@@ -23,6 +24,7 @@ function Universe() {
           <img
             src="media/images/zerodhafundhouse.png"
             style={{ height: "44px" }}
+            alt="zerodhafundhouse"
           />
           <p className="text-small text-muted fs-6">
             Our asset management venture that is creating simple and transparent
@@ -33,6 +35,7 @@ function Universe() {
           <img
             src="media/images/sensibullLogo.svg"
             style={{ height: "44px" }}
+            alt="sensibullLogo"
           />
           <p className="text-small text-muted fs-6">
             Options trading platform that lets you create strategies, analyze
@@ -41,21 +44,33 @@ function Universe() {
           </p>
         </div>
         <div className="col-4 p-3 mt-5">
-          <img src="media/images/streakLogo.png" style={{ height: "44px" }} />
+          <img
+            src="media/images/streakLogo.png"
+            style={{ height: "44px" }}
+            alt="streakLogo"
+          />
           <p className="text-small text-muted fs-6">
             Systematic trading platform that allows you to create and backtest
             strategies without coding.
           </p>
         </div>
         <div className="col-4 p-3 mt-5">
-          <img src="media/images/dittoLogo.png" style={{ height: "44px" }} />
+          <img
+            src="media/images/dittoLogo.png"
+            style={{ height: "44px" }}
+            alt="dittoLogo"
+          />
           <p className="text-small text-muted fs-6">
             Personalized advice on life and health insurance. No spam and no
             mis-selling. Sign up for free
           </p>
         </div>
         <div className="col-4 p-3 mt-5">
-          <img src="media/images/tijori.svg" style={{ height: "44px" }} />
+          <img
+            src="media/images/tijori.svg"
+            style={{ height: "44px" }}
+            alt="tijori"
+          />
           <p className="text-small text-muted fs-6">
             Investment research platform that offers detailed insights on
             stocks, sectors, supply chains, and more.
