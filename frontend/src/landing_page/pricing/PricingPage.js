98dd7294a666ce5b94de14619e9014da6d7bc862
bodhekarvarad <bodhekarvarad2004@gmail.com>
2026-06-01 22:42:34 +0530

    add a pricing

---
+++ frontend/src/landing_page/pricing/PricingPage.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Hero from "./Hero";
+import OpenAccount from "../OpenAccount";
+import Brojkarage from "./Brojkarage";
+function PricingPage() {
+  return (
+    <div>
+      <Hero />
+      <OpenAccount />
+      <Brojkarage />
+    </div>
+  );
+}
+
+export default PricingPage;
