17bf774817e140552dbd77064b61f081bc671c92
bodhekarvarad <bodhekarvarad2004@gmail.com>
2026-06-05 15:06:09 +0530

    connected mongodb

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,12 @@
 const express = require("express");
 const app = express();
+require("dotenv").config();
+const mongoose = require("mongoose");
+const PORT = process.env.PORT || 4444;
+const url = process.env.MONGO_URL;
 
 app.listen(4444, () => {
   console.log("app started");
+  mongoose.connect(url);
+  console.log("dbconnected");
 });
