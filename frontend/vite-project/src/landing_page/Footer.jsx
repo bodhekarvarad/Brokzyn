53dc05fa55f4778fe64ea303c171d93751da2c64
bodhekarvarad <bodhekarvarad2004@gmail.com>
2026-06-16 22:11:47 +0530

    add frontend

---
+++ frontend/vite-project/src/landing_page/Footer.jsx
@@ -0,0 +1,179 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer style={{ backgroundColor: "rgb(248, 249, 250)" }}>
+      <div className="container border-top  mt-5">
+        <div className="row mt-5">
+          <div className="col">
+            <img
+              src="media/images/logo.svg"
+              style={{ width: "50%" }}
+              alt="Logo"
+            />
+            <p>&copy;2010 - 2026,Brokzyn Broking Ltd. All rights reserved.</p>
+          </div>
+          <div className="col">
+            <p>Company</p>
+            <a href="#" style={{ textDecoration: "none" }}>
+              {" "}
+              About
+            </a>
+            <br />
+            <a href="#" style={{ textDecoration: "none" }}>
+              {" "}
+              Philosophy
+            </a>
+            <br />
+            <a href="#" style={{ textDecoration: "none" }}>
+              {" "}
+              Press & media
+            </a>
+            <br />
+            <a href="#" style={{ textDecoration: "none" }}>
+              {" "}
+              Careers
+            </a>
+            <br />
+            <a href="#" style={{ textDecoration: "none" }}>
+              {" "}
+              Brokzyn Cares (CSR)
+            </a>
+            <br />
+            <a href="#" style={{ textDecoration: "none" }}>
+              {" "}
+              Brokzyn.tech
+            </a>
+            <br />
+            <a href="#" style={{ textDecoration: "none" }}>
+              {" "}
+              Open source
+            </a>
+            <br />
+            <a href="#" style={{ textDecoration: "none" }}>
+              {" "}
+              Referral program
+            </a>
+          </div>
+          <div className="col">
+            <p>Support</p>
+            <a href="#" style={{ textDecoration: "none" }}>
+              {" "}
+              Contact us
+            </a>
+            <br />
+            <a href="#" style={{ textDecoration: "none" }}>
+              {" "}
+              Support portal
+            </a>
+            <br />
+            <a href="#" style={{ textDecoration: "none" }}>
+              {" "}
+              How to file a complaint?
+            </a>
+            <br />
+            <a href="#" style={{ textDecoration: "none" }}>
+              {" "}
+              Status of your complaints
+            </a>
+            <br />
+            <a href="#" style={{ textDecoration: "none" }}>
+              {" "}
+              Bulletin
+            </a>
+            <br />
+            <a href="#" style={{ textDecoration: "none" }}>
+              {" "}
+              Circular
+            </a>
+            <br />
+            <a href="#" style={{ textDecoration: "none" }}>
+              {" "}
+              Z-Connect blog
+            </a>
+            <br />
+            <a href="#" style={{ textDecoration: "none" }}>
+              {" "}
+              Downloads
+            </a>
+          </div>
+          <div className="col">
+            <p>Account</p>
+            <a href="#" style={{ textDecoration: "none" }}>
+              {" "}
+              Open demat account
+            </a>
+            <br />
+            <a href="#" style={{ textDecoration: "none" }}>
+              {" "}
+              Minor demat account
+            </a>
+            <br />
+            <a href="#" style={{ textDecoration: "none" }}>
+              {" "}
+              NRI demat account
+            </a>
+            <br />
+            <a href="#" style={{ textDecoration: "none" }}>
+              {" "}
+              HUF demat account
+            </a>
+            <br />
+            <a href="#" style={{ textDecoration: "none" }}>
+              {" "}
+              Commodity
+            </a>
+            <br />
+            <a href="#" style={{ textDecoration: "none" }}>
+              {" "}
+              Dematerialisation
+            </a>
+            <br />
+            <a href="#" style={{ textDecoration: "none" }}>
+              {" "}
+              Fund transfer MTF
+            </a>
+          </div>
+        </div>
+        <div className="mt-5  text-muted" style={{ fontSize: "14px" }}>
+          <p>
+            Procedure to file a complaint on SEBI SCORES: Register on SCORES
+            portal. Mandatory details for filing complaints on SCORES: Name,
+            PAN, Address, Mobile Number, E-mail ID. Benefits: Effective
+            Communication, Speedy redressal of the grievances
+          </p>
+          <p>
+            Investments in securities market are subject to market risks; read
+            all the related documents carefully before investing. Attention
+            investors: 1) Stock brokers can accept securities as margins from
+            clients only by way of pledge in the depository system w.e.f
+            September 01, 2020. 2) Update your e-mail and phone number with your
+            stock broker / depository participant and receive OTP directly from
+            depository on your e-mail and/or mobile number to create pledge. 3)
+            Check your securities / MF / bonds in the consolidated account
+            statement issued by NSDL/CDSL every month.
+          </p>
+          <p>
+            Prevent unauthorised transactions in your account. Update your
+            mobile numbers/email IDs with your stock brokers/depository
+            participants. Receive information of your transactions directly from
+            Exchange/Depositories on your mobile/email at the end of the day.
+            Issued in the interest of investors. KYC is one time exercise while
+            dealing in securities markets - once KYC is done through a SEBI
+            registered intermediary (broker, DP, Mutual Fund etc.), you need not
+            undergo the same process again when you approach another
+            intermediary." Dear Investor, if you are subscribing to an IPO,
+            there is no need to issue a cheque. Please write the Bank account
+            number and sign the IPO application form to authorize your bank to
+            make payment in case of allotment. In case of non allotment the
+            funds will remain in your bank account. As a business we don't give
+            stock tips, and have not authorized anyone to trade on behalf of
+            others. If you find anyone claiming to be part of Brokzyn and
+            offering such services, please create a ticket here.
+          </p>
+        </div>
+      </div>
+    </footer>
+  );
+}
+export default Footer;
---
+++ frontend/vite-project/src/landing_page/product/RightSection.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+function RightSection({
+  imageUrl,
+  productName,
+  productDescription,
+  learnMore,
+}) {
+  return (
+    <div className="container mt-5">
+      <div className="row">
+        <div className="col-6 p-5 mt-5">
+          <h1>{productName}</h1>
+          <p>{productDescription}</p>
+          <div className="links">
+            <a href={learnMore} style={{ textDecoration: "none" }}>
+              Learn More <i class="fa-solid fa-arrow-right-long"></i>
+            </a>
+          </div>
+        </div>
+        <div className="col-6">
+          <img src={imageUrl} alt={productName} />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default RightSection;
